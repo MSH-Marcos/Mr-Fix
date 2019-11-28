a1220d86408726f8edd24f7f274558817430a930
MSH-Marcos <marcos.solaz.hernandez@gmail.com>
2019-11-28 17:38:08 +0100

    implement user controller

--- src/main/java/com/msh/mrfix/controllers/UserController.java
+++ src/main/java/com/msh/mrfix/controllers/UserController.java
@@ -1,13 +1,63 @@
 package com.msh.mrfix.controllers;
 
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.msh.mrfix.models.User;
+import com.msh.mrfix.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @ControllerAdvice
 @RestController
 @RequestMapping("${mrfix.version}")
 public class UserController {
 
-    
+    @Autowired
+    UserRepository userR;
+
+    @GetMapping("/users")
+    public List<User> list(){
+        return userR.findAllNotDeleted();
+    }
+
+    @GetMapping("/users/{id}")
+    public User getUser(@PathVariable String id){
+        return userR.findById(Long.parseLong(id)).orElse(null);
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> add(@RequestBody User userToAdd) {
+        try{
+            userR.save(userToAdd);
+        }catch (IllegalArgumentException e){
+            return ResponseEntity.noContent().build();
+        }
+
+        return new ResponseEntity<User>(userToAdd, HttpStatus.OK);
+    }
+
+    @PutMapping("/users/{id}")
+    public ResponseEntity<User> update(@PathVariable String id, @RequestBody User userToUpdate) {
+        int result = userR.updateUser(Long.parseLong(id), userToUpdate.getAddress(), userToUpdate.getName(),
+                userToUpdate.getSurname(), userToUpdate.getEmail());
+
+        if (result == 0) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return new ResponseEntity<User>(userToUpdate, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<User> delete(@PathVariable String id) {
+        int result = userR.deleteUser(Long.parseLong(id));
+
+        if (result != 1) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return new ResponseEntity<User>(getUser(id), HttpStatus.OK);
+    }
 }
--- src/main/java/com/msh/mrfix/repositories/UserRepository.java
+++ src/main/java/com/msh/mrfix/repositories/UserRepository.java
@@ -2,6 +2,27 @@ package com.msh.mrfix.repositories;
 
 import com.msh.mrfix.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User,Long> {
+
+    @Query("SELECT u FROM User u WHERE u.deleted = false ")
+    public List<User> findAllNotDeleted();
+
+    @Modifying(clearAutomatically = true)
+    @Transactional
+    @Query("UPDATE User u SET u.address = :address, u.name = :name, u.surname = :surname, u.email = :email WHERE u.id = :userId")
+    public int updateUser(@Param("userId") long userId, @Param("address") String address,
+                   @Param("name") String name, @Param("surname") String surname,
+                   @Param("email") String email);
+
+    @Modifying(clearAutomatically = true)
+    @Transactional
+    @Query("UPDATE User u SET u.deleted = true WHERE u.id = :userId")
+    public int deleteUser(@Param("userId") long userId);
 }
