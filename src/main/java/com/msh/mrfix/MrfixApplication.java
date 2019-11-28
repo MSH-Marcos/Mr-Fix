c04e2e6c6ca146d9334f3a54cc1b31daaf6a7db5
MSH-Marcos <marcos.solaz.hernandez@gmail.com>
2019-11-28 15:03:10 +0100

    init

---
+++ src/main/java/com/msh/mrfix/MrfixApplication.java
@@ -0,0 +1,13 @@
+package com.msh.mrfix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrfixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MrfixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/msh/mrfix/MrfixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msh.mrfix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrfixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
