44492dbbd502d0d860439b0b15c12d85cfd9963a
MSH-Marcos <marcos.solaz.hernandez@gmail.com>
2019-11-28 17:39:34 +0100

    fix order

--- src/main/java/com/msh/mrfix/models/Order.java
+++ src/main/java/com/msh/mrfix/models/Order.java
@@ -3,16 +3,15 @@ package com.msh.mrfix.models;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "services")
+@Table(name = "orders")
 public class Order {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
-    private String description;
     private float price;
+
 
     public Long getId() {
         return id;
@@ -20,22 +19,6 @@ public class Order {
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 
     public float getPrice() {
