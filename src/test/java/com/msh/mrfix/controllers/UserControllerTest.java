ed1f29967ada4e9ea15869d21f90c46202dece4d
MSH-Marcos <marcos.solaz.hernandez@gmail.com>
2019-12-03 13:25:58 +0100

    clean user test

--- src/test/java/com/msh/mrfix/controllers/UserControllerTest.java
+++ src/test/java/com/msh/mrfix/controllers/UserControllerTest.java
@@ -1,27 +1,18 @@
 package com.msh.mrfix.controllers;
 
-import com.msh.mrfix.models.User;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.http.HttpHeaders;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
-import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.methods.RequestBuilder;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.LaxRedirectStrategy;
-import org.apache.http.message.BasicNameValuePair;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
