00ea06f99cd5ba19e7207f395d5ccd469e952acf
marcosS <marcos.solaz.hernandez@gmail.com>
2019-12-08 12:56:23 +0100

    add authenticaton filter

---
+++ src/main/java/com/msh/mrfix/security/JwtAuthenticationFilter.java
@@ -0,0 +1,73 @@
+package com.msh.mrfix.security;
+
+import com.msh.mrfix.helpers.SecurityConstants;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+        setFilterProcessesUrl("/login");
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        System.out.println(request.getParameter("username") + "  " + request.getParameter("password"));
+        try {
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                    request.getParameter("username"), request.getParameter("password"));
+
+            return authenticationManager.authenticate(authenticationToken);
+        } catch (AuthenticationException ex) {
+            throw new ResponseStatusException(
+                    HttpStatus.UNAUTHORIZED, "Incorrect nick/password", ex);
+        }
+    }
+
+    @Override
+    public void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain,
+                                         Authentication authentication) throws IOException, ServletException {
+
+        User user = (User) authentication.getPrincipal();
+
+        List<String> roles = user.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
+
+        String token = Jwts.builder()
+                .signWith(Keys.hmacShaKeyFor(SecurityConstants.SIGNING_KEY), SignatureAlgorithm.HS512)
+                .setHeaderParam("type", "jwt")
+                .setIssuer(SecurityConstants.ISSUER_TOKEN)
+                .setAudience("secuer-app")
+                .setSubject(user.getUsername())
+                .setExpiration(new Date(System.currentTimeMillis() + SecurityConstants.ACCESS_TOKEN_VALIDITY_SECONDS))
+                .claim("rol", roles)
+                .compact();
+
+        response.addHeader(SecurityConstants.HEADER_AUTHORIZATION_KEY, SecurityConstants.TOKEN_BEARER_PREFIX + token);
+
+    }
+}
